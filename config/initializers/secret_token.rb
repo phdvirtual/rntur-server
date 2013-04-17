eb05a80ba48578102645ab2228539aedeee24fdb
Marcelo Gomes <chelogui@gmail.com>
2013-04-17 01:39:35 -0300

    Commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RnturServer::Application.config.secret_token = 'ef8c0d4173822d7cc83934d7283b97aacb21e3fa9d94ed053babd9bef892f149a3f222ad22d94b62927f13e2326631db821df80acca5457d2a44f644fa516b2a'
